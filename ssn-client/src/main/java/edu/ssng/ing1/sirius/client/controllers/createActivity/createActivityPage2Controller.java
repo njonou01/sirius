060a38493a632d30f1c287f0c615b4a705fd92e1
MichelEloka <michel.eloka@etu.u-pec.fr>
2024-04-05 00:51:38 +0100

    update page2

--- ssn-client/src/main/java/edu/ssng/ing1/sirius/client/controllers/createActivity/createActivityPage2Controller.java
+++ ssn-client/src/main/java/edu/ssng/ing1/sirius/client/controllers/createActivity/createActivityPage2Controller.java
@@ -1,12 +1,15 @@
 package edu.ssng.ing1.sirius.client.controllers.createActivity;
 
 import java.net.URL;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 import edu.ssng.ing1.sirius.client.router.Router;
 import edu.ssng.ing1.sirius.client.router.RouterPoUp;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -27,7 +30,7 @@ public class createActivityPage2Controller implements Initializable {
     TextField dateDebutTextField;
 
     @FXML
-    Label label;
+    private Label label;
 
     @FXML
     private Slider slider;
@@ -36,7 +39,13 @@ public class createActivityPage2Controller implements Initializable {
     private Label labelTexteField;
 
     @FXML
-    Label labelCount;
+    private Label labelCount;
+
+    @FXML
+    private DatePicker datePickerBegin;
+
+    @FXML
+    private DatePicker datePickerEnd;
 
     private int maxLength = 3;
 
@@ -68,7 +77,8 @@ public class createActivityPage2Controller implements Initializable {
             if (newValue) {
 
                 labelCount.setOpacity(0.5);
-                slider.setOpacity(0.5); 
+                slider.setOpacity(0.5);
+                RouterPoUp.activite.setNbrparticipant(Integer.valueOf(nbrParticipantTextField.getText())); 
             } else {
                 labelCount.setOpacity(1.0);
                 slider.setOpacity(1.0); 
@@ -79,7 +89,8 @@ public class createActivityPage2Controller implements Initializable {
         slider.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 labelTexteField.setOpacity(0.5);
-                nbrParticipantTextField.setOpacity(0.5); 
+                nbrParticipantTextField.setOpacity(0.5);
+                RouterPoUp.activite.setNbrparticipant((int) slider.getValue());
             } else {
                 labelTexteField.setOpacity(1.0);
                 nbrParticipantTextField.setOpacity(1.0); 
@@ -88,6 +99,22 @@ public class createActivityPage2Controller implements Initializable {
 
         slider.valueProperty().addListener((observable, oldValue, newValue) -> {
             labelCount.setText(""+newValue.intValue());
+        });
+
+        datePickerBegin.setOnAction(event -> {
+            LocalDate localDate = datePickerBegin.getValue();
+            if (localDate != null) {
+                Timestamp timestamp = Timestamp.valueOf(localDate.atStartOfDay());
+                System.out.println("Timestamp : " + timestamp);
+            }
+        });
+
+        datePickerEnd.setOnAction(event -> {
+            LocalDate localDate = datePickerEnd.getValue();
+            if (localDate != null) {
+                Timestamp timestamp = Timestamp.valueOf(localDate.atStartOfDay());
+                System.out.println("Timestamp : " + timestamp);
+            }
         });
 
     }
