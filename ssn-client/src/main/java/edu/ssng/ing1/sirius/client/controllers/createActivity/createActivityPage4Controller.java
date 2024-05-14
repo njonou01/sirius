e2800eda7f793fd6ab4f8c49ac67128de93b88f4
MichelEloka <michel.eloka@etu.u-pec.fr>
2024-05-14 17:45:25 +0200

    Upgrade style

--- ssn-client/src/main/java/edu/ssng/ing1/sirius/client/controllers/createActivity/createActivityPage4Controller.java
+++ ssn-client/src/main/java/edu/ssng/ing1/sirius/client/controllers/createActivity/createActivityPage4Controller.java
@@ -17,9 +17,15 @@ import edu.ssng.ing1.sirius.client.toast.ToastType;
 // import edu.ssng.ing1.sirius.MainInsertClient;
 
 import edu.ssng.ing1.sirius.requests.activities.InsertActivityQuery;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * createActivityPage2Controller
@@ -29,19 +35,42 @@ public class createActivityPage4Controller implements Initializable {
 
     Button overBtn;
 
+    
+
+    @FXML
+    Button inviteFriendsBtn;
+
+    @FXML
+    Button lastActivityBtn;
+
+    @FXML
+    VBox friendVBox;
+    
+    @FXML
+    VBox inviteVbox;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         router = RouterPopUp.getInstance();
+
+        Platform.runLater(() -> {
+            Student student = UserInfo.getUser();
+            lastActivityBtn.setOnAction(event -> displayFriendsInvite(student));
+        });
 
     }
 
     @FXML
     public void insertActivity() {
         Student student = UserInfo.getUser();
-        long timestamp = System.currentTimeMillis(); // Par exemple
+        long timestamp = System.currentTimeMillis(); 
         Timestamp ts = new Timestamp(timestamp);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String timestampString = sdf.format(ts);
+
+        
+
+
         RouterPopUp.activite.setId_student(student.getId_student());
         RouterPopUp.activite.setNomCreateur(student.getFirstname());
         RouterPopUp.activite.setProvenance("HomePage");
@@ -66,6 +95,65 @@ public class createActivityPage4Controller implements Initializable {
 
         router.getStage().close();
 
+    }
+
+    public void displayFriendsInvite(Student student){
+        Platform.runLater(() -> {
+            System.out.println("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+            System.out.println("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+            System.out.println("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+
+
+
+            Label labelInvite= new Label("Amis Invité");
+            labelInvite.getStyleClass().add("InvitSend");
+
+
+            HBox leftBoxStudent = new HBox();
+            leftBoxStudent.getStyleClass().add("leftBoxStudent");
+            leftBoxStudent.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
+            leftBoxStudent.setPrefHeight(77.0);
+            leftBoxStudent.setPrefWidth(254.0);
+
+            ImageView imageView = new ImageView();
+            imageView.setFitHeight(65.0);
+            imageView.setFitWidth(60.0);
+            imageView.setPickOnBounds(true);
+            imageView.setPreserveRatio(true);
+
+            VBox friendVBox = new VBox();
+            friendVBox.setAlignment(javafx.geometry.Pos.CENTER);
+            friendVBox.setPrefHeight(77.0);
+            friendVBox.setPrefWidth(200.0);
+            friendVBox.setSpacing(20.0);
+            friendVBox.setId("friendVBox");
+
+            Label label = new Label("Nom :" +student.getFamilyname()+ " prenom :"+student.getFirstname());
+
+            Button inviteFriendsBtn = new Button("Inviter");
+            inviteFriendsBtn.setId("inviteFriendsBtn");
+            inviteFriendsBtn.getStyleClass().add("btn");
+            inviteFriendsBtn.setMnemonicParsing(false);
+
+            
+
+            friendVBox.getChildren().addAll(label, inviteFriendsBtn);
+            int index= friendVBox.getChildren().indexOf(inviteFriendsBtn);
+
+            
+            leftBoxStudent.getChildren().addAll(imageView, friendVBox);
+            inviteFriendsBtn.setOnAction(event->{
+                friendVBox.getChildren().set(index, labelInvite);
+            });
+
+            inviteVbox.getChildren().add(friendVBox);
+
+
+        });
+            
+            
+            
+       
     }
 
 }
