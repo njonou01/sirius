8f2e4de463b25d3c3e6a6e683454f70bbbafc808
njonou01 <njonougaby01@gmail.com>
2024-05-17 12:08:29 +0200

    test notif

--- ssn-client/src/main/java/edu/ssng/ing1/sirius/client/notificationManagement/business/SsnNotifyService.java
+++ ssn-client/src/main/java/edu/ssng/ing1/sirius/client/notificationManagement/business/SsnNotifyService.java
@@ -12,10 +12,13 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.ssng.ing1.sirius.business.dto.Activite;
+import edu.ssng.ing1.sirius.business.dto.BeFriend;
 import edu.ssng.ing1.sirius.business.dto.Message;
+import edu.ssng.ing1.sirius.business.dto.Student;
 import edu.ssng.ing1.sirius.client.controllers.commons.HomeController;
 import edu.ssng.ing1.sirius.client.controllers.commons.Initializer;
 import edu.ssng.ing1.sirius.client.controllers.createActivity.SeeMyActivityController;
+import edu.ssng.ing1.sirius.client.controllers.friend.FriendUpdater;
 import edu.ssng.ing1.sirius.client.controllers.messaging.MessagingUpdater;
 import edu.ssng.ing1.sirius.commons.Notification;
 import javafx.application.Platform;
@@ -47,8 +50,43 @@ public class SsnNotifyService {
                 break;
             case "DENY_INVITATION":
                 denyActivity(Notify);
+            case "ACCEPTED_INVITATION":
+                notifyAcceptedInvitation(Notify);
+                break;
+            case "DEMAND_INVITATION":
+                notifyDemandFriend(Notify);
+
+                break;
             default:
                 break;
+        }
+    }
+
+    private void notifyDemandFriend(Notification notification) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+            BeFriend friend = mapper.readValue(notification.getBody(), BeFriend.class);
+            friend.setStatus("no reponse");
+            Initializer.getFriends().add(friend);
+        } catch (IOException e) {
+            LoggerFactory.getLogger(SsnNotifyService.class).error("error", e);
+        }
+
+    }
+
+    private void notifyAcceptedInvitation(Notification notification) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+            BeFriend friend = mapper.readValue(notification.getBody(), BeFriend.class);
+            friend.setStatus("accepted");
+            Initializer.getFriends().add(friend);
+            FriendUpdater.updateAcceptedFriend();
+            FriendUpdater.updateLimitedDemandedFriend();
+           
+        } catch (IOException e) {
+            LoggerFactory.getLogger(SsnNotifyService.class).error("error", e);
         }
     }
 
